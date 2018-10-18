c67cb65620fadcfff8730e1c2ee8746f22ef94c4
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-10-18 15:08:27 +0300

    add comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { MyInterceptor } from './core/interceptors/my.interceptor';
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    // Add .withServerTransition() to support Universal rendering.
+    // The application ID can be any identifier which is unique on
+    // the page.
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     FormsModule,
     HttpClientModule,
