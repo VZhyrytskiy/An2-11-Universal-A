3a3c51463c430a706b4ecb4ffa1ca62e2e73aa57
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-10-18 16:09:33 +0300

    remove ModuleMapLoaderModule

--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
 
 import { AppModule } from './app.module';
 import { AppComponent } from './app.component';
@@ -10,9 +9,7 @@ import { AppComponent } from './app.component';
     // The AppServerModule should import your AppModule followed
     // by the ServerModule from @angular/platform-server.
     AppModule,
-    ServerModule,
-
-    ModuleMapLoaderModule // <-- *Important* to have lazy-loaded routes work
+    ServerModule
   ],
   // Since the bootstrapped component is not inherited from your
   // imported AppModule, it needs to be repeated here.
