afec4040714dcfdd8cc62828032f98d8f87be4a4
Vitaliy Zhyrytskyy <Vitaliy_Zhyrytskyy@epam.com>
2018-10-30 14:44:51 +0200

    t05

--- src/app/tasks/tasks-routing.module.ts
+++ src/app/tasks/tasks-routing.module.ts
@@ -3,20 +3,49 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { TaskListComponent, TaskFormComponent } from './components';
 
+const metaTags = [
+  {
+    name: 'description',
+    content: 'Task Manager Application. This is an ASP application'
+  },
+  {
+    name: 'keywords',
+    content: 'Angular 7 tutorial, SPA Application, Routing'
+  },
+  {
+    name: 'twitter:title',
+    content: 'Angular 7 tutorial, SPA Application, Routing'
+  },
+  {
+    name: 'twitter:image',
+    content:
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png'
+  },
+  {
+    name: 'twitter:url',
+    content: 'https://my-site.my-hosting.com'
+  },
+  {
+    name: 'twitter:description',
+    content: 'Task Manager Application. This is an ASP application'
+  },
+  {
+    name: 'twitter:card',
+    content: 'summary'
+  },
+  {
+    name: 'twitter:site',
+    content: '@Tutor'
+  }
+];
+
 const routes: Routes = [
   {
     path: 'home',
     component: TaskListComponent,
     data: {
       title: 'Task Manager',
-      meta: [{
-        name: 'description',
-        content: 'Task Manager Application. This is an ASP application'
-      },
-      {
-        name: 'keywords',
-        content: 'Angular 4 tutorial, SPA Application, Routing'
-      }]
+      meta: metaTags
     }
   },
   {
